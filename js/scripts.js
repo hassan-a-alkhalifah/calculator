782b285048fceeaf4810db58b6813d18a40ce2e2
Ben Kiggen and Hassan Al-khalifah <ben@>
2018-08-06 10:37:58 -0700

    initial commit - basic back end finished

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var add = function(number1, number2) {
+  return parseInt(number1) + parseInt(number2);
+};
+var subtract = function(number1, number2) {
+  return parseInt(number1) - parseInt(number2);
+};
+var divide = function(number1, number2) {
+  return parseInt(number1) / parseInt(number2);
+};
+var multiply = function(number1, number2) {
+  return parseInt(number1) * parseInt(number2);
+};
+
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+
+alert(add(num1, num2));
