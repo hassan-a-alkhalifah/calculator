68f20cda3a0edd28cab92bda6199395b025b0bab
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-09 23:13:10 -0700

    reformated javascript, and initial conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Business (or back-end) logic:
-var userInput= "";
-var input1 = "";
-var input2 = "";
+var userInput = "";
+var digit1 = "";
+var digit2 = "";
 var operator = "";
-var input = 0;
-var inputArray = [];
-var output = "";
-var result = 0;
-// var add = function(userInput) {
-//   return number1 + number2;
-// };
+var total = "";
+
+var add = function(num1, num2) {
+  console.log("num1: " + num1 + " num2: " + num2);
+  total = parseInt(num1) + parseInt(num2);
+  console.log(total);
+};
 // var subtract = function(userInput) {
 //   return number1 - number2;
 // };
@@ -20,54 +20,49 @@ var result = 0;
 //   return number1 / number2;
 // };
 
+var calculate = function(oper) {
+  if (oper === "+") {
+    console.log("prior to add method " + oper + " " + digit1 + " " + digit2);
+    add(digit1, digit2);
+  } else if (oper === "-") {
+    subtract(digit1, digit2);
+  } else if (oper === "x") {
+    multiply(digit1, digit2);
+  } else if (oper === "÷") {
+    divide(digit1, digit2);
+  }
+};
+
 // User interface (or front-end) logic:
 
 $(document).ready(function() {
-  
-
   $("button").click(function() {
     userInput = $(this).val();
-    if(userInput >= "0" && userInput <= "9") {
-      if (input1 === "") {
-        input1 += userInput;
-      } else {
-        input2 += userInput;
+    if ( userInput >= 0 || userInput <= 9 ) {
+      if ( operator === "") {
+        digit1 += userInput;
+        console.log("input 1 " + digit1);
+      } else if ( operator !== "" && digit2 === "" ) {
+        digit2 += userInput;
+        console.log("input 2 " + digit2);
       }
     } else {
-      calculate(userInput);
-    }
-
-
-    if (operator === "(") {
-      output = "";
-    } else if (operator === ")") {
-      output = "";
-    } else if (operator === "ac") {
-      output = "";
-    } else if (operator === "÷") {
-      output = "";
-    } else if (operator === "x") {
-      output = "";
-    } else if (operator === "-") {
-      output = "";
-    } else if (operator === "+") {
-      if (input1 && input2) {
-
+      if (operator === "" && digit2 === "") {
+        operator = userInput;
+        console.log("operator prior to digit2 " + operator);
+      } else if ( operator !== "" ** digit2 !== "" ) {
+        console.log("operator prior to calculate " + operator);
+        calculate(operator);
+        console.log("After calculate: operator: " + operator + " digit1: " + digit1 + " digit2: " + digit2);
       }
-      input = 0;
-      output = "";
-    } else {
-      output += $(this).val();
-      input += userInput;
     }
-    $("#user-input").val(output);
   });
 
-  $("calculator").submit(function(event) {
-    event.preventDefault();
-    // userInput = parseInt($("#user-input").val());
-
-    // result = add(userInput);
-    // $("#output").text(result);
-  });
+  // $("calculator").submit(function(event) {
+  //   event.preventDefault();
+  //   userInput = parseInt($("#user-input").val());
+  //
+  //   result = add(userInput);
+  //   $("#output").text(result);
+  // });
 });
